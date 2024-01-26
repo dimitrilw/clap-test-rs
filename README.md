54bfb29b1c118ca4aa6f9d7e21e8369f0f73b034
Dimitri <5898931+dimitrilw@users.noreply.github.com>
2024-01-26 16:21:12 -0700

    complete & publish demo external process call
    Signed-off-by: Dimitri <5898931+dimitrilw@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # clap-test-rs
+
 Repo for testing Rust clap crate.
+
+## Usage
+
+Clone repo, then `cargo build`, which will create a binary at `target/debug/ctr`.
+
+Ensure that binary is in your PATH. An easy solution is to copy it to `/usr/local/bin`.
+
+Ensure that `ctr-asdfblah` is in your PATH. An easy solution is to copy it to `/usr/local/bin`.
+
+When both `which ctr` and `which ctr-asdfblah` return a path, you're ready to go.
+
+Run the test via `ctr asdfblah arg1 arg2`. Your output should look like:
+
+```
+from ctr binary :: Calling out to "ctr-asdfblah" with ["arg1", "arg2"]
+hello from executable file 'ctr-asdfblah' :)
+this file received the args: arg1 arg2
+
+about to call 'ctr clone AsdfBlah_ITS_ALIVE'
+ctr call starting...
+
+from ctr binary :: Cloning AsdfBlah_ITS_ALIVE
+
+...ctr call ended
+goodbye from executable file 'ctr-asdfblah'  o/
+```
+
+This output shows the ctr binary calling out to the ctr-asdfblah binary,
+which then calls back to the ctr binary.
+This pattern allows users to write their own extensions to the ctr app.
+This is how the external-process of `git` works; i.e., I can create a file on my path called
+`git-foo` and then call `git foo` and the git binary will call out to my `git-foo` binary.
+That `git-foo` binary can then do whatever I need, including a call back to the original `git`.
+This allows something like `git-co` to be written, which is a common alias for `git checkout`.
+The `git-co` file would simply execute `git checkout $@` to pass along any arguments it received.
+Instant alias, but with the option of more fully powered extensions, and not _just_ an alias.
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+// taken from the Cookbook's git_derive example, then pared down for testing
+// https://docs.rs/clap/latest/clap/_derive/_cookbook/git_derive/index.html
+
+use std::ffi::OsString;
+use std::process;
+
+use clap::{Parser, Subcommand};
+
+/// A test CLI
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "ctr")]
+#[command(about = "A test CLI", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Clones repos
+    #[command(arg_required_else_help = true)]
+    Clone {
+        /// The remote to clone
+        remote: String,
+    },
+
+    // more commands here
+
+    #[command(external_subcommand)]
+    External(Vec<OsString>),
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Clone { remote } => {
+            println!("from ctr binary :: Cloning {remote}");
+        }
+        // more commands here
+        Commands::External(args) => {
+            let external_executable = format!("ctr-{}", &args[0].to_str().unwrap());
+            let ext_exe_args = &args[1..];
+
+            let msg = format!(
+                "from ctr binary :: failed to execute external process '{:?}' with args '{:?}'",
+                &external_executable,
+                &ext_exe_args,
+            );
+            let msg_str = msg.as_str();
+
+            println!(
+                "from ctr binary :: Calling out to {:?} with {:?}", 
+                &external_executable, 
+                &ext_exe_args,
+            );
+
+            process::Command::new(external_executable)
+                .args(ext_exe_args)
+                .status()
+                .expect(msg_str);
+        }
+    }
+}
